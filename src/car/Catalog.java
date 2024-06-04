e7e77aa31f09560751a3ebde0817c8499b9e70c9
Riteesh19 <riteeshbavireddy@gmail.com>
2024-06-04 00:09:54 -0400

    initial commit

---
+++ src/car/Catalog.java
@@ -0,0 +1,165 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car;
+
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author riteesh
+ */
+public class Catalog {
+    
+    private String Company;
+    private String Model;
+    private String Color;
+    private String Year;
+    private String EngineNumber;
+    private String SeatingCapacity; 
+    private String LicensePlate;
+    private String OwnerName;
+    private String OwnerTelephone;
+    private String OwnerEmail;
+    private String OwnerLicense;
+    private String OwnerSSN;
+    private String OwnerAddress;
+    private String ServiceRecords;
+    private String WarrantyYear;
+
+    public String getCompany() {
+        return Company;
+    }
+
+    public void setCompany(String Company) {
+        this.Company = Company;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public void setYear(String Year) {
+        this.Year = Year;
+    }
+
+    public String getEngineNumber() {
+        return EngineNumber;
+    }
+
+    public void setEngineNumber(String EngineNumber) {
+        this.EngineNumber = EngineNumber;
+    }
+
+    public String getSeatingCapacity() {
+        return SeatingCapacity;
+    }
+
+    public void setSeatingCapacity(String SeatingCapacity) {
+        this.SeatingCapacity = SeatingCapacity;
+    }
+
+    public String getLicensePlate() {
+        return LicensePlate;
+    }
+
+    public void setLicensePlate(String LicensePlate) {
+        this.LicensePlate = LicensePlate;
+    }
+
+    public String getOwnerName() {
+        return OwnerName;
+    }
+
+    public void setOwnerName(String OwnerName) {
+        this.OwnerName = OwnerName;
+    }
+
+    public String getOwnerTelephone() {
+        return OwnerTelephone;
+    }
+
+    public void setOwnerTelephone(String OwnerTelephone) {
+        this.OwnerTelephone = OwnerTelephone;
+    }
+
+    public String getOwnerEmail() {
+        return OwnerEmail;
+    }
+
+    public void setOwnerEmail(String OwnerEmail) {
+        this.OwnerEmail = OwnerEmail;
+    }
+
+    public String getOwnerLicense() {
+        return OwnerLicense;
+    }
+
+    public void setOwnerLicense(String OwnerLicense) {
+        this.OwnerLicense = OwnerLicense;
+    }
+
+    public String getOwnerSSN() {
+        return OwnerSSN;
+    }
+
+    public void setOwnerSSN(String OwnerSSN) {
+        this.OwnerSSN = OwnerSSN;
+    }
+
+    public String getOwnerAddress() {
+        return OwnerAddress;
+    }
+
+    public void setOwnerAddress(String OwnerAddress) {
+        this.OwnerAddress = OwnerAddress;
+    }
+
+    public String getServiceRecords() {
+        return ServiceRecords;
+    }
+
+    public void setServiceRecords(String ServiceRecords) {
+        this.ServiceRecords = ServiceRecords;
+    }
+
+    public String getWarrantyYear() {
+        return WarrantyYear;
+    }
+
+    public void setWarrantyYear(String WarrantyYear) {
+        this.WarrantyYear = WarrantyYear;
+    }
+
+    public String getPhoto() {
+      
+        return null;
+      
+    }
+
+    public void validLicenseNumber(String text) {
+    }
+   
+    }
+    
+     
+
+
